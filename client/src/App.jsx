e41f4e7c816a1fa5ed2318e3a3116c541261e34b
sneha <neha06844@gmail.com>
2025-02-08 17:49:30 +0530

    react

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <nav className='bg-emerald-950 flex py-5 px-10 items-center justify-between'>
+    <h2 className=' text-2xl'>sheryans</h2>
+    <div className='flex gap-8 items-center'>
+      <h4 className='text-xl'>About</h4>
+      <h4 className='text-xl'>Contact</h4>
+      <h4 className='text-xl'>Services</h4>
+      <h4 className='text-xl'>Your Account</h4>
+    </div>
+    </nav>
+    </>
+  )
+}
+
+export default App
